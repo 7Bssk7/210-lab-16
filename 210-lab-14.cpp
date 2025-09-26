5d375b58d8353a733a47ad1efed94ebd2442ce7e
7Bssk7 <arhip.fin3@gmail.com>
2025-09-25 19:06:13 -0700

    Update

--- 210-lab-14.cpp
+++ 210-lab-14.cpp
@@ -2,7 +2,23 @@
 
 using namespace std;
 
+class Colors {
+public: 
+    void setRed(int r) { this->red = r; }
+    int getRed() {return red; }
+    void setGreen(int g) {this->green = g; }
+    int getGreen() {return green; }
+    void setBlue(int b) {this->blue = b; }
+    int getBlue() {return blue; }
+
+private:
+    int red, green, blue;
+
+};
+
 int main(){
+
+    Colors color;
 
     return 0;
 }
